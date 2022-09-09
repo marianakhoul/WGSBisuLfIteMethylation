b78f7d503a40c725d9b172a1a775af16295f2436
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-09-09 19:41:03 -0400

    Update dmrAnnotation.R

--- docker/r_docker/scripts/dmrAnnotation.R
+++ docker/r_docker/scripts/dmrAnnotation.R
@@ -1,5 +1,24 @@
 #!/usr/bin/env Rscript
 
+library(optparse)
+library(Rserve)
+Rserve(args="--no-save")
+
+
+option_list <- list(
+                make_option(c("--bed_graphs"), type = "character", help = "bed_graphs file"),
+                make_option(c("--output"), type = "character", help = "methylation_metrics output"),
+                make_option(c("--meth_rate_on_chr"), type = "float",default="0.1", help = "methylation rate on chromosome"))
+
+parseobj <- OptionParser(option_list=option_list, usage = "usage: Rscript %prog [options]")
+opt <- parse_args(parseobj)
+args <- commandArgs(TRUE)
+options(stringsAsFactors=FALSE, width=160, scipen=999)
+
+bed_graphs<-opt$bed_graphs
+output<-opt$output
+rate<-opt$meth_rate_on_chr
+
 
 wgbs.annotateDMRs <- function (dmrFile, gzippedCgiFile, gzippedGeneFile, gzippedRepeatMaskerAnnotationFile, gzippedCoverageFiles, allowedBiotypes, promoterTSSDistances, annotatedDmrFile) {
 
@@ -24,10 +43,7 @@ wgbs.annotateDMRs <- function (dmrFile, gzippedCgiFile, gzippedGeneFile, gzipped
   return(dmrs)
 }
 
-if (exists("snakemake")) {
-
-   # save.image(file = "dmr-annotation-snakemake.Rdata")
-   wgbs.annotateDMRs(
+wgbs.annotateDMRs(
      snakemake@input$combined_dmrs,
      snakemake@params$cgi_annotation_file,
      snakemake@params$gene_annotation_file,
@@ -38,4 +54,3 @@ if (exists("snakemake")) {
      snakemake@output$annotated_dmrs
    )
 
-}
