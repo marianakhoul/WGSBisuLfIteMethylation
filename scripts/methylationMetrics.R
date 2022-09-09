f09d3442a258699cf036855f1e8323d91ca82c62
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-09-09 14:25:06 -0400

    Update methylationMetrics.R

--- scripts/methylationMetrics.R
+++ scripts/methylationMetrics.R
@@ -1,10 +1,25 @@
 #!/usr/bin/env Rscript
 
-
+library(optparse)
 library(Rserve)
+library(data.table)
 Rserve(args="--no-save")
 
-library(data.table)
+
+option_list <- list(
+                make_option(c("--bed_graphs"), type = "character", help = "bed_graphs file"),
+                make_option(c("--output"), type = "character", help = "methylation_metrics output"),
+                make_option(c("--meth_rate_on_chr"), type = "integer",default="0.1", help = "methylation rate on chromosome"))
+
+parseobj <- OptionParser(option_list=option_list, usage = "usage: Rscript %prog [options]")
+opt <- parse_args(parseobj)
+args <- commandArgs(TRUE)
+options(stringsAsFactors=FALSE, width=160, scipen=999)
+
+bed_graphs<-opt$bed_graphs
+output<-opt$output
+rate<-opt$meth_rate_on_chr
+
 
 wgbs.methylation.computeMeanMethylation <- function(methylationTable) {
 
@@ -56,13 +71,6 @@ wgbs.methylation.computeMethylationMetrics <- function(bedGraphFiles, methylatio
   return(methylationMetrics)
 }
 
-if (exists("snakemake")) {
-  
-  # save.image("conversion-snakemake.Rdata")
-  
-  wgbs.methylation.computeMethylationMetrics(
-    snakemake@input$bed_graphs,
-    snakemake@output$methylation_metrics,
-    snakemake@params$methylation_rate_on_chromosomes
-  )
-}
+
+wgbs.methylation.computeMethylationMetrics(bed_graphs,output,rate)
+
